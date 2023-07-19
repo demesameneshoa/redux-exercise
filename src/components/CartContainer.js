26ae73623cf932f293116e5482b0f8517c8cf26c
demesameneshoa <demesameneshoa@gmail.com>
2023-07-19 16:44:28 +0300

    add useselector to components

---
+++ src/components/CartContainer.js
@@ -0,0 +1,38 @@
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
+const CartContainer = () => {
+    const {cartItems, total, amount} = useSelector ((store) => store.cart)
+    if (amount < 1){
+        return <section className="cart">
+            <header>
+                <h2> Your bag</h2>
+                <h4 className="empty-cart">is currently empty</h4>
+            </header>
+        </section>
+    }
+    
+    return(
+        <section className="cart">
+        <header>
+            <h2>Your Bag</h2>
+        </header>
+        <div>
+            {cartItems.map((item)=> {
+            return <CartItem key={item.id} {...item} />;
+            })}
+        </div>
+        <div className="cart-total">
+        <footer>
+            <hr />
+            <h4>
+            Total <span>${total}</span>
+            </h4>
+        <button className="btn clear-btn">Clear Cart</button>
+       </footer>
+    </div>
+        
+    </section> 
+    ); 
+};
+
+export default CartContainer;
